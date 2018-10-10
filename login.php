0e2b4cc02c1cd32a625c584a53663cb280bb6802
1337 <obrequel337@gmail.com>
2018-10-10 14:29:08 -0300

    primer login admin/admin

--- login.php
+++ login.php
@@ -30,7 +30,7 @@
         </label>
         
       </div>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Iniciar Sesion</button>
+      <button class="btn btn-lg btn-primary btn-block" type="submit" name="submit">Iniciar Sesion</button>
      	<p class="mt-3"><a href="crear.php">Crear Usuario</a></p>
       <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
     </form>
