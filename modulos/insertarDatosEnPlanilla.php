e1a4f6c71e8d840b72e22f7b5fe6c90b5db4c19b
edurikelm <riquelme.mayorga@gmail.com>
2018-11-07 14:53:27 -0300

    insert funcionando de los datos de llenar planilla pie

--- modulos/insertarDatosEnPlanilla.php
+++ modulos/insertarDatosEnPlanilla.php
@@ -4,24 +4,49 @@ require_once ("../configs/conexion_db.php");
 
 if(isset($_POST['insertar'])){
 
-    $clicks = mysqli_escape_string($enlace, $_POST['bntAdd']);
+
     $selectUno = mysqli_escape_string($enlace, $_POST['nombre1']);
     $selectDos = mysqli_escape_string($enlace, $_POST['nombre2']);
     $selectTres = mysqli_escape_string($enlace, $_POST['nombre3']);
     $selectCuatro = mysqli_escape_string($enlace, $_POST['nombre4']);
     $selectCinco = mysqli_escape_string($enlace, $_POST['nombre5']);
-    $bucle_profe =mysqli_escape_string($enlace, $_POST['bucle_profe']);
+    $bucle_profe = mysqli_escape_string($enlace, $_POST['bucle_profe']);
+    $numero = mysqli_escape_string($enlace, $_POST['numero']);
 
 
 }
 
+// echo ("el valor del select es ".$bucle_profe);
+// echo $numero;
 
-echo ("el valor del select es ".$bucle_profe);
+if($bucle_profe==1){
+
+    $sql = "INSERT INTO usuarios_planilla (id_planilla, id_profesional) VALUES ('".$numero."','".$selectUno."')";
+
+}elseif($bucle_profe==2){
+
+    $sql = "INSERT INTO usuarios_planilla (id_planilla, id_profesional) VALUES ('".$numero."','".$selectUno."'), ('".$numero."','".$selectDos."')";
+
+}elseif($bucle_profe==3){
+
+    $sql = "INSERT INTO usuarios_planilla (id_planilla, id_profesional) VALUES ('".$numero."','".$selectUno."'), ('".$numero."','".$selectDos."'), ('".$numero."','".$selectTres."')";
+
+}elseif($bucle_profe==4){
+
+    $sql = "INSERT INTO usuarios_planilla (id_planilla, id_profesional) VALUES ('".$numero."','".$selectUno."'), ('".$numero."','".$selectDos."'), ('".$numero."','".$selectTres."'), ('".$numero."','".$selectCuatro."')";
+
+}elseif($bucle_profe==5){
+
+    $sql = "INSERT INTO usuarios_planilla (id_planilla, id_profesional) VALUES ('".$numero."','".$selectUno."'), ('".$numero."','".$selectDos."'), ('".$numero."','".$selectTres."'), ('".$numero."','".$selectCuatro."'), ('".$numero."','".$selectCinco."')";
+
+}
+
 
 if(mysqli_query($enlace, $sql)):
     // $_SESSION['mensaje'] = '<div class="alert alert-success alert-dismissible" role="alert">
     // <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
     // Alumno agregado con exito!</div>';
+    header('Location: ../pages/llenar_plantilla_pie.php');
     echo "agregado OK";
     
 else:
