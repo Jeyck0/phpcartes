a3c43592d7aac6e816e7956e52f6e8ba87021c60
edurikelm <riquelme.mayorga@gmail.com>
2018-11-29 19:58:26 -0300

    alguna cosa se cambio

--- pages/agregar_objetivos.php
+++ pages/agregar_objetivos.php
@@ -4,9 +4,6 @@ include ('../configs/conexion_db.php');
 
 if(isset($_GET['id'])): 
     $id_planilla = mysqli_escape_string($enlace, $_GET['id']);
-
-
-
 endif;
 ?>
 
@@ -14,29 +11,26 @@ endif;
     <div class="row">
         <div class="col-lg-12">
             <h1 class="page-header">Registro de apoyos para cada estudiante o grupo de estudiantes</h1>
-            <p>Registrar, por estudiante o grupos de estudiantes, los apoyos específicos o actividades especiales que
-                se realizan en
-                forma individual o en pequeños grupos dentro o fuera del aula regular y el o las/os nombres de los
-                profesionales que
-                los entregan</p>
+            <p>Registrar, por estudiante o grupos de estudiantes, los apoyos específicos o actividades especiales que se realizan en
+            forma individual o en pequeños grupos dentro o fuera del aula regular y el o las/os nombres de los profesionales que
+            los entregan</p>
         </div>
         <!-- /.col-lg-12 -->
     </div>
-    <div class="row">
-        <form id="" role="form" method="POST" action="../modulos/insertar_objetivos.php">
-            <div class="col-lg-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        Datos
-                    </div>
-                    <div class="panel-body">
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <div class="form-group">
-                                    <label for="">Profesional</label>
-                                    <select name="profesional" id="profesional" class="form-control">
-                                        <option value=""> Seleccione </option>
-                                        <?php
+    <div class="row"><form id="" role="form" method="POST" action="../modulos/insertar_objetivos.php">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Datos
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-lg-6">     
+                            <div class="form-group">
+                                <label for="">Profesional</label>
+                                <select name="profesional" id="profesional" class="form-control">
+                                    <option value=""> Seleccione </option>
+                                    <?php
                                     $sql = "SELECT up.id_profesional,p.nombres,p.apellidos,p.titulo_profesional FROM usuarios_planilla up INNER JOIN profesionals p ON up.id_profesional=p.id  WHERE id_planilla = '$id_planilla'";
                                     $resultado = mysqli_query($enlace, $sql);
                                     while ($dado = mysqli_fetch_array($resultado)):
@@ -51,57 +45,46 @@ endif;
                                         <?php
                                     endwhile;
                                 ?>
-                                    </select>
-                                </div>
-                                <div class="form-group">
-                                    <label for="">Lugar (Dentro o fuera del aula)</label>
-                                    <select name="lugar_aula" id="lugar_aula" class="form-control">
-                                        <option value="" selected>Seleccione</option>
-                                        <option value="1">Aula Comun</option>
-                                        <option value="2">Aula de Recursos</option>
-                                    </select>
-                                </div>
-                                <div class="form-group">
-                                    <label for="">Horas Realizadas</label>
-                                    <input type="text" name="horas" class="form-control">
-                                </div>
-                                <div class="form-group">
-                                    <label for="">Acciones, actividades y apoyos entregados a estudiantes.</label>
-                                    <textarea name="txt_actividades" id="txt_actividades" rows="3" class="form-control"
-                                        maxlength="130" placeholder="Hasta 130 caracteres..."></textarea>
-                                </div>
-                                <div class="form-group">
-                                    <label for="">Observaciones / Acuerdos</label>
-                                    <textarea name="txt_observaciones" id="txt_observaciones" rows="3" class="form-control"
-                                        maxlength="130" placeholder="Hasta 130 caracteres..."></textarea>
-                                </div>
-                                <div class="form-group">
-                                    <input type="submit" name="insertar_objetivos" id="insertar_objetivos" value="Insertar a planilla"
-                                        class="btn btn-lg btn-primary" />
-                                    <input type="hidden" name='id' value="<?php echo $id_planilla; ?>">
-                                </div>
+                                </select>
                             </div>
-          
-                            <div class="col-lg-6">
-                                <div class="form-group">
-                                    <a href="agregar_resumen.php?id=<?php echo $id_planilla; ?>" type="submit" name="btn-ver"
-                                        class="btn btn-success btn-lg btn-block">Agregar Resumen</a>
-                                </div>
+                            <div class="form-group">
+                                <label for="">Lugar (Dentro o fuera del aula)</label>
+                                <select name="lugar_aula" id="lugar_aula" class="form-control">
+                                    <option value="" selected>Seleccione</option>
+                                    <option value="1">Aula Comun</option>
+                                    <option value="2">Aula de Recursos</option>
+                                </select>
                             </div>
-                            <div class="col-lg-6">
-                                <div class="form-group">
-                                    <a href="editar_resumen.php?id=<?php echo $id_planilla; ?>" type="submit" name="btn-editar"
-                                        class="btn btn-warning btn-lg btn-block">Editar Resumen</a>
-                                </div>
+                            <div class="form-group">
+                                <label for="">Horas Realizadas</label>
+                                <input type="text" name="horas" class="form-control">
                             </div>
+                            <div class="form-group">
+                                <label for="">Acciones, actividades y apoyos entregados a estudiantes.</label>
+                                <textarea name="txt_actividades" id="txt_actividades"  rows="3" class="form-control" maxlength="130" placeholder="Hasta 200 caracteres..."></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label for="">Observaciones / Acuerdos</label>
+                                <textarea name="txt_observaciones" id="txt_observaciones"  rows="3" class="form-control" maxlength="130" placeholder="Hasta 200 caracteres..."></textarea>
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" name="insertar_objetivos" id="insertar_objetivos" value="Insertar a planilla" class="btn btn-lg btn-primary" />
+                                <input type="hidden" name = 'id' value="<?php echo $id_planilla; ?>">
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                            <a href="agregar_resumen.php?id=<?php echo $id_planilla; ?>" type="submit" name="btn-ver" class="btn btn-success btn-lg btn-block">Agregar Resumen</a>       
+                            </div>            
                         </div>
                     </div>
                 </div>
-        </form>
+            </div>
+</form></div>
     </div>
-</div>
 </div>
 <?php
 include ('includes/cierre-interfaz.php');
 ?>
+
 
